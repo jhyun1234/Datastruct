e06617ab4e3e4f38b091530ebaebf97fcbec710f
jhyun1234 <anjyounl24@gmail.com>
2023-10-10 19:48:18 +0900

    2023.10.10 단일 연결 리스트

---
+++ Datastruct/Datastruct.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+
+using namespace std;
+#pragma region 단일 연결 리스트
+
+    // 1. 단일 연결 리스트 노드 제작 
+   
+struct Node
+{
+    int data;
+    Node* next;
+
+
+};
+
+
+
+#pragma endregion
+
+void PushFront(Node * target, int data)
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = target->next;
+    target->next = newNode;
+
+    
+}
+
+
+int main()
+{
+    Node* head = new Node;
+    head->next = nullptr;
+
+    PushFront(head, 10);
+    PushFront(head, 20);
+
+
+    Node* currentPtr = head->next;
+
+    while (currentPtr != nullptr)
+    {
+        cout << currentPtr->data << endl;
+        currentPtr = currentPtr->next;
+    }
+
+    
+}
+
