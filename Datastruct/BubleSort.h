de441cbf021b7bfcf811593b51998fb65d23688f
jhyun1234 <anjyounl24@gmail.com>
2023-10-19 19:46:25 +0900

    2023.10.19 선택정렬

---
+++ Datastruct/BubleSort.h
@@ -0,0 +1,154 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define SIZE 5
+
+#pragma region 연산자 오버로딩
+class Vector2
+{
+private:
+    int x;
+    int y;
+public:
+    Vector2(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+
+    }
+
+    Vector2 operator+(const Vector2& vector2)
+    {
+        Vector2 clone(this->x + vector2.x, this->y + vector2.y);
+
+        return clone;
+    }
+    Vector2 operator-(const Vector2& vector2)
+    {
+        Vector2 clone(this->x - vector2.x, this->y - vector2.y);
+
+        return clone;
+    }
+    Vector2 operator*(const Vector2& vector2)
+    {
+        Vector2 clone(this->x * vector2.x, this->y * vector2.y);
+
+        return clone;
+    }
+    Vector2 operator/(const Vector2& vector2)
+    {
+        Vector2 clone(this->x / vector2.x, this->y / vector2.y);
+
+        return clone;
+    }
+
+    Vector2 operator++()
+    {
+        this->x++;
+        this->y++;
+
+        return *this;
+    }
+
+    Vector2 operator++(int)
+    {
+
+        //Vector2 temp(this->x = x + 1, this->y = y + 1);
+
+        Vector2 clone = *this;
+
+        this->x++;
+        this->y++;
+
+
+
+        return clone;
+
+
+
+    }
+
+
+    // ++
+    // --
+    // []
+
+
+    int GetX()
+    {
+        return x;
+    }
+    int GetY()
+    {
+        return y;
+    }
+};
+
+
+
+#pragma endregion
+
+
+
+int main()
+{
+#pragma region 거품 정렬
+    // 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘이다.
+
+    // 시간 복잡도 O(n^2) 
+
+
+    /*int bubbleBuffer[SIZE] = { 1,3,4,7,2 };
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < (SIZE-i) - 1; j++)
+        {
+            if (bubbleBuffer[j] > bubbleBuffer[j + 1])
+            {
+                swap(bubbleBuffer[j], bubbleBuffer[j + 1]);
+            }
+        }
+
+    }
+
+    for (const int& element : bubbleBuffer)
+    {
+        cout << element << " ";
+    }*/
+
+
+#pragma endregion
+
+#pragma region 연산자 오버로딩
+
+
+
+
+
+    Vector2 UP(0, 1);
+    Vector2 Right(1, 0);
+
+
+    // clone 객체 (1,1)
+    Vector2 temp = UP + Right;
+
+    Vector2 a = temp++;
+
+
+    cout << a.GetX() << endl;
+    cout << a.GetY() << endl;
+
+    temp++;
+
+    cout << temp.GetX() << endl;
+    cout << temp.GetY() << endl;
+#pragma endregion
+
+
+
+}
+
--- Datastruct/Datastruct.cpp
+++ Datastruct/Datastruct.cpp
@@ -1,112 +1,49 @@
 ﻿#include <iostream>
-#include <vector>
 
 using namespace std;
 
 #define SIZE 5
-
-#pragma region 연산자 오버로딩
-class Vector2
-{
-private:
-    int x;
-    int y;
-public:
-    Vector2(int x, int y)
-    {
-        this->x = x;
-        this->y = y;
-
-    }
-
-    Vector2 operator+(const Vector2 & vector2)
-    {
-        Vector2 clone(this->x + vector2.x , this->y + vector2.y);
-        
-        return clone;
-    }
-    Vector2 operator-(const Vector2& vector2)
-    {
-        Vector2 clone(this->x - vector2.x, this->y - vector2.y);
-
-        return clone;
-    }
-    Vector2 operator*(const Vector2& vector2)
-    {
-        Vector2 clone(this->x * vector2.x, this->y * vector2.y);
-
-        return clone;
-    }
-    Vector2 operator/(const Vector2& vector2)
-    {
-        Vector2 clone(this->x / vector2.x, this->y / vector2.y);
-
-        return clone;
-    }
-
-
-    // ++
-    // --
-    // []
-
-
-    int GetX()
-    {
-        return x;
-    }
-    int GetY()
-    {
-        return y;
-    }
-};
-
-
-
-#pragma endregion
-
-
-
 int main()
 {
-#pragma region 거품 정렬
-    // 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘이다.
+#pragma region 선택 정렬
 
-    // 시간 복잡도 O(n^2) 
+    // 정렬되지 않은 데이터들에 대해 가장 작은 데이터를
+    // 찾아서 가장 앞에 있는 데이터와 교환하는 알고리즘이다.
+    
+    // 시간 복잡도 O(n^2)
 
 
-    /*int bubbleBuffer[SIZE] = { 1,3,4,7,2 };
+    int SelectBuffer[] = { 1,3,4,7,2 };
 
-    for (int i = 0; i < SIZE; i++)
+    int size = sizeof(SelectBuffer) / sizeof(int);
+
+    int temp;
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < (SIZE-i) - 1; j++)
-        {
-            if (bubbleBuffer[j] > bubbleBuffer[j + 1])
-            {
-                swap(bubbleBuffer[j], bubbleBuffer[j + 1]);
-            }
-        }
+        int min = SelectBuffer[i]; // 1
+        
+        int select = i;
 
+        for (int j = i + 1; j < size; j++)
+        {
+
+            if (min > SelectBuffer[j])
+            {
+
+                min = SelectBuffer[j];
+                select = j;
+
+            }
+
+
+        }
+        std::swap(SelectBuffer[i], SelectBuffer[select]);
     }
 
-    for (const int& element : bubbleBuffer)
+    for (int& elemt : SelectBuffer)
     {
-        cout << element << " ";
-    }*/
-
-
-#pragma endregion
-
-#pragma region 연산자 오버로딩
-
-
-
-
-    Vector2 UP(0, 1);
-    Vector2 Right(1, 0);
-
-
-    // clone 객체 (1,1)
-    Vector2 temp = UP + Right;
+        cout << elemt << " ";
+    }
 
 #pragma endregion
 
