bf6126a5b3a0f85678afd385e5f172fe9e80e36e
jhyun1234 <anjyounl24@gmail.com>
2023-11-01 19:48:18 +0900

    2023.11.1 이진 탐색 트리 삭제

--- Datastruct/BinarySearchTree.h
+++ Datastruct/BinarySearchTree.h
@@ -1,15 +1,13 @@
 #pragma once
+
 #include <iostream>
 
 using namespace std;
 
 #pragma region 이진 탐색 트리 (BST)
 // 임의의 키를 가진 원소를 삽입, 삭제, 검색하는데
-// 효율적인 알고리즘이다.
-
-
-#pragma endregion
-
+// 효율적인 알고리즘입니다.
+#pragma region 트리
 // 편향 이진 트리
 // 하나의 차수로만 이루어져 있는 트리
 
@@ -20,49 +18,131 @@ using namespace std;
 // 완전 이진 트리
 // 모든 노드가 왼쪽부터 차근차근 생성되는 이진 트리
 
+#pragma endregion
 
-
-
-
+#pragma endregion
 
 template <typename T>
 class BinarySearchTree
 {
 private:
-	struct Node
-	{
-		T data;
-		Node* left;
-		Node* right;
-	};
-
-
-	Node* rootNode;
+    struct Node
+    {
+        T data;
+        Node* left;
+        Node* right;
+    };
 
 public:
+    Node* rootNode;
 
-	BinarySearchTree()
-	{
-		rootNode = nullptr;
-	}
+    BinarySearchTree()
+    {
+        rootNode = nullptr;
+    }
 
-	Node* GetRoot()
-	{
-		return rootNode;
-	}
+    Node* GetRoot()
+    {
+        return rootNode;
+    }
 
-	void Insert(T data, Node* root)
-	{
-		if (root == nullptr)
-		{
-			root = new Node<T>;
-			root->data = data;
-			root->left = nullptr;
-			root->right = nullptr;
-		}
+    Node* Insert(T data, Node* root)
+    {
+        if (Find(root, data) == true)
+        {
+            cout << "Duplicate Key" << endl;
+            return root;
+        }
+        if (root == nullptr)
+        {
+            root = new Node;
+            root->data = data;
+            root->left = nullptr;
+            root->right = nullptr;
+        }
+        
+        else if (root->data > data) // root의 data보다 작다면?
+        {
+            root->left = Insert(data, root->left);
+        }
+        else
+        {
+            root->right = Insert(data, root->right);
+        }
+        
+        return root;
+    }
+
+    bool Find(Node* root, T data)
+    {
+        if (root == nullptr)
+        {
+            return false;
+        }
+        if (root->data == data)
+        {
+            return true;
+        }
+        else
+        {
+            if (root->data > data)
+            {
+                return Find(root->left, data);
+            }
+            else
+            {
+                return Find(root->right, data);
+            }
+        }
+    }
 
 
-	}
+    T& FindMax(Node* root)
+    {
+        if (root == nullptr)
+        {
+            cout << "Binary Search Tree is Empty" << endl;
+            exit(1);
+        }
 
+        if (root->right == nullptr)
+        {
+            return root->data;
+        }
+        else
+        {
+            return FindMax(root->right);
+        }
+    }
+
+    T& FindMin(Node* root)
+    {
+        if (root == nullptr)
+        {
+            cout << "Binary Search Tree is Empty" << endl;
+            exit(1);
+        }
+
+        if (root->left == nullptr)
+        {
+            return root->data;
+        }
+        else
+        {
+            return FindMin(root->left);
+        }
+    }
+
+    void Inorder(Node* root)
+    {
+        if (root != nullptr)
+        {
+            Inorder(root->left);
+            cout << root->data << " ";
+            Inorder(root->right);
+        }
+    }
+
+    
 
 };
--- Datastruct/Datastruct.cpp
+++ Datastruct/Datastruct.cpp
@@ -1,12 +1,25 @@
 ﻿#include <iostream>
+#include"BinarySearchTree.h"
 using namespace std;
+
 
 
 
 int main()
 {
+	BinarySearchTree<int>bst;
+
+	
+	bst.rootNode = bst.Insert(10, bst.rootNode);
+	bst.Insert(10, bst.GetRoot());
+	bst.Insert(5, bst.GetRoot());
+	bst.Insert(15, bst.GetRoot());
 
 
+	cout<<bst.FindMax(bst.rootNode) << endl;
+
+	
+	bst.Inorder(bst.GetRoot());
 	return 0;
 
 
