6fe9c1b74e845866a2de5be74a1576d4e50c1b0d
KIM <anjyounl24@gmail.com>
2023-11-06 19:48:09 +0900

    2023.11.6 해시테이블

--- Datastruct/HashTable.h
+++ Datastruct/HashTable.h
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// template <typename T> 
+#define SIZE 5
 
 #pragma region 해시 테이블
 // Key,Value 로 데이터를 저장하는 자료 구조 중 하나로
@@ -30,3 +30,95 @@ using namespace std;
 //            도출하는 방식이다.
 
 #pragma endregion
+
+
+template <typename T, typename V>
+class HashTable
+{
+private:
+	struct Node
+	{
+		T key;
+		V value;
+		Node* next;
+
+	};
+	struct Bucket
+	{
+		Node* head;
+		int count;
+
+	};
+	
+	
+	Bucket bucket[SIZE];
+public:
+	HashTable()
+	{
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
+		}
+
+	}
+
+	int HashFunction(T key)
+	{
+		int sumValue = 0;
+		if (T == string) 
+		{
+			for (const char& element : key)
+			{
+				sumValue += (int)element;
+
+			}
+			int hashIndex = sumValue % SIZE;
+
+			return hashIndex;
+		}
+		
+	}
+
+	Node* CreatNode(T key, V value)
+	{
+		Node* newNode = new Node;
+		
+		newNode->key = key;
+		newNode->value = value;
+		newNode->next = nullptr;
+		return newNode;
+	}
+	void Insert(T key, V value)
+	{
+		// 해시 함수를 통해서 값을 받는 임시 변수
+		int hashIndex = HashFunction(key);
+
+		// 새로운 노드를 생성한다.
+		Node* newNode = CreatNode(key, value);
+		
+		// 노드가 1개 라도 존재하지 않는다면
+		if (bucket[hashIndex].count == 0)
+		{
+			// 1. buket[hashIndex]의 head 포인터에 새로운 노드의 주소값을 저장한다.
+			bucket[hashIndex].head = newNode;
+
+			// 2. buket[hashIndex]의 count 변수의 값을 증가시킨다.
+			bucket[hashIndex].count++;
+
+		}
+		else // 노드가 1개라도 존재한다면
+		{
+			// 1. NewNode의 next에 bucket[hashIndex]의 head의 값을 넣어준다.
+			newNode->next = bucket[hashIndex].head;
+
+			// 2. bucket[hashIndex].head를 방금 새로 생성한 노드의 주소를 가르키게 한다.
+			bucket[hashIndex].head = newNode;
+
+			// 3. bucket[hashIndex].count 의 값을 증가시킨다.
+			bucket[hashIndex].count++;
+		}
+	}
+};
+
--- Datastruct/STACK.cpp
+++
@@ -1,1 +0,0 @@
-#include "STACK.h"
